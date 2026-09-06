6af6a4571d3318868234b094ee0eaf4338cc1ecb
Vision Linux Team <visionlinux@users.noreply.github.com>
2026-09-06 16:20:31 +0500

    Fix build: add iso_version, fix workflow paths

--- profiles/visionlinux/profiledef.sh
+++ profiles/visionlinux/profiledef.sh
@@ -1,14 +1,12 @@
 # Vision Linux archiso profile
 
 profile_archiso_version=80
-profile_type="baseline"
 
 iso_publisher="Vision Linux <team@visionlinux.dev>"
 iso_application="Vision Linux"
-iso_version="$(date +%Y.%m.%d)"
+iso_version="2026.09.06"
 iso_label="VISIONLINUX"
 iso_directory="visionlinux"
-iso_filename="visionlinux-${iso_version}-x86_64.iso"
 
 bootloader="limine"
 kernel="linux-lts"
